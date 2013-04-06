e807609fc5d883e69d84f56558818d03565e8c87
hochitom <hochitom@icloud.com>
2013-04-06 22:22:49 +0200

    realtime stream, frontent improvements

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,19 +1,26 @@
 var socket = io.connect('http://127.0.0.1:3001');
 
-
-
 socket.on('connect', function () {
-    socket.on('tweet', function (msg) {
-        console.log(msg);
-        var template = '<div class="media">';
-            template += '<a class="pull-left" href="#">';
-            template += '<img class="media-object" src="' + msg.user.profile_image_url +'" data-src="holder.js/64x64">';
-            template += '</a>';
-            template += '<div class="media-body">';
-            template += '<h4 class="media-heading">@' + msg.user.screen_name + '</h4>';
-            template += '<p>' + msg.text + '</p>';
-            template += '</div></div>';
+    
+});
 
-        $('#stream').prepend(template);
+socket.on('tweet', function (msg) {
+    console.log(msg);
+    var template = '<li id="' + msg.id + '"><div class="media">';
+        template += '<a class="pull-left" href="#">';
+        template += '<img class="media-object" src="' + msg.user.profile_image_url +'" data-src="holder.js/64x64">';
+        template += '</a>';
+        template += '<div class="media-body">';
+        template += '<h4 class="media-heading">@' + msg.user.screen_name + '</h4>';
+        template += '<p>' + msg.text + '</p>';
+        template += '</div></div></li>';
+
+    $('#stream').prepend(template);
+});
+
+$('#tweeting').on('submit', function(data) {
+    $.post('/tweet', {msg: $('#tweet').val()}, function(data){
+        $('#myModal').modal('hide');
     });
+    return false;
 });
--- routes/stream.js
+++ routes/stream.js
@@ -2,6 +2,7 @@ var express = require('express'),
     OAuth = require('oauth').OAuth,
     twitter = require('ntwitter'),
     io = require('socket.io').listen(3001),
+    https = require('https'),
     mapper = require('../lib/model-mapper');
 
 
@@ -12,7 +13,7 @@ module.exports = function(app) {
         access_token = "19526041-YcTQ9Thw8OzBnhlog6EPjKBdF5tc4oDn3Q3YHzvH0",
         access_token_secret = "Yx1xxc7jEV4VxCmUXGryZAjxyFtVBN1EwAkUHWAIg5o";
     
-    app.get('/stream', function(req, res) {
+    app.get('/', function(req, res) {
         var twit = new twitter({
             consumer_key: user_key,
             consumer_secret: user_secret,
@@ -22,13 +23,45 @@ module.exports = function(app) {
 
         twit.stream('user', function(stream, error) {
             if (error) console.error(error);
+
             io.sockets.on('connection', function (socket) {
                 stream.on('data', function (data) {
                     socket.emit('tweet', data);
                 });
+
+                socket.on('disconnect', function () {
+                    io.sockets.emit('user disconnected');
+                });
             });
         });
 
-        res.render('pages/stream');
+        oa = new OAuth("https://twitter.com/oauth/request_token",
+                 "https://twitter.com/oauth/access_token", 
+                 user_key, user_secret, 
+                 "1.0A", "http://localhost:3000/", "HMAC-SHA1");
+
+        oa.get("http://api.twitter.com/1/statuses/home_timeline.json", access_token, access_token_secret, function(error, data) {
+            res.render('pages/stream', {tweets: JSON.parse(data)});
+        });
+    });
+
+    app.post('/tweet', function(req, res) {
+        var twit = new twitter({
+            consumer_key: user_key,
+            consumer_secret: user_secret,
+            access_token_key: access_token,
+            access_token_secret: access_token_secret
+        });
+
+        twit
+            .verifyCredentials(function (err, data) {
+                console.log(data);
+            }).updateStatus(req.body.msg, function (err, data) {
+                if (err) {
+                    console.error(err);
+                    res.send(415);
+                }
+                console.log(data);
+            });
     });
 }
