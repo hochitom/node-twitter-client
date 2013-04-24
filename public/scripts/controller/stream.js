9b3ed07caa9fa19b91c22e980a56e7123ea04dcb
hochitom <hochitom@icloud.com>
2013-04-24 20:13:22 +0200

    update scope

--- public/scripts/controller/stream.js
+++ public/scripts/controller/stream.js
@@ -1,20 +1,27 @@
 (function () {
     app.controller("StreamCtrl", function StreamCtrl ($scope, $rootScope, $http, Push) {
+        $scope.tweets;
 
         $http
             .get('/stream')
             .then(function (res) {
+                $scope.tweets =res.data;
             });
 
         $scope.addTweet = function (data) {
             $scope.tweets.push(data);
-            console.log($scope.tweets.length);
+            $scope.$digest('tweets');
             console.log($scope.tweets);
         };
 
+        $scope.$watch('tweets', function () {
+            console.log('tt');
+        });
+
         $rootScope.$on('new-tweet', function (event, data) {
-            if (!data.friends) {
             if (data.friends) {
+                $scope.friends = data.friends;
+            } else {
                 $scope.addTweet(data);
             }
         });
