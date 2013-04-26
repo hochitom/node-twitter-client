d99b217ba8d218d82b91e77925916a2266213e34
Thomas Hochoertler <t.hochoertler@diamonddogs.cc>
2013-04-26 08:32:07 +0200

    fixed tweet pimping, added user detection

--- public/scripts/directives/pimptweets.js
+++ public/scripts/directives/pimptweets.js
@@ -4,26 +4,24 @@
             console.log(scope);
             console.log(elems);
 
-            var tweet = scope.tweet.text,
-                links = parseFloat(attrs.links),
+            var links = parseFloat(attrs.links),
                 user = parseFloat(attrs.user_mentions),
                 symbols = parseFloat(attrs.symbols),
                 hashtags = parseFloat(attrs.hashtags);
-
-            console.log(tweet);
             
             if (links > 0) {
                 var link_exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-                scope.tweet.text = tweet.replace(link_exp,"<a href='$1' target='_blank'>$1</a>");
+                scope.tweet.text = scope.tweet.text.replace(link_exp,"<a href='$1' target='_blank'>$1</a>");
             }
             
             if (user > 0) {
-                console.log('user: ' + user);
+                var user_exp = /@([a-zA-Z0-9]+)/g;
+                scope.tweet.text = scope.tweet.text.replace(user_exp,"<a href='https://twitter.com/$1' target='_blank'>@$1</a>");
             }
             
             if (hashtags > 0) {
                 var hashtag_exp = /#([a-zA-Z0-9]+)/g;
-                scope.tweet.text = tweet.replace(hashtag_exp,"<a href='https://twitter.com/search?q=$1&src=hash' target='_blank'>#$1</a>");
+                scope.tweet.text = scope.tweet.text.replace(hashtag_exp,"<a href='https://twitter.com/search?q=$1&src=hash' target='_blank'>#$1</a>");
             }
             
             if (symbols > 0) {
